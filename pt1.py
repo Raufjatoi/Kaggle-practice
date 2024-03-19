c0b85023811ee7368fc5cff5d068d5788bcd0e74
Abdul rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-03-19 11:34:41 +0500

    Update pt1.py

--- pt1.py
+++ pt1.py
@@ -5,4 +5,9 @@ import numpy as np
 
  ## Dataframe
 
- print(pd.Dataframe(data=np.arrange(0,20).reshape(5,4),index=['row1','row2','row3','row4','row5'],colums=['colum1','colum2','colum3','colum4']))
+info = pd.DataFrame(data=np.arrange(0,20).reshape(5,4),index=['row1','row2','row3','row4','row5'],column=['colum1','colum2','colum3','colum4'])
+#print(info.info())
+# indexing 
+#use iloc and loc
+
+#its not workin here so i gona use jupyter
